6bdbcdd957931f09218ffcf5d55caf4902b14fef
dvir <dvirla84@gmail.com>
2022-05-08 18:29:52 +0300

    Added parallel_test_V2.py

---
+++ MCTS_V2.py
@@ -0,0 +1,144 @@
+from copy import deepcopy
+from BayesBeta import BayesBeta
+import numpy as np
+from scipy.stats import bernoulli
+from itertools import product
+
+np.seterr(divide='ignore', invalid='ignore')
+
+
+class MCTSNode:
+    def __init__(self, actions_history, parent=None):
+        self.children = {}
+        self.actions_history = actions_history
+        self.parent = parent
+        self.N = 0
+        self.N_per_action = np.zeros((2, 2))  # 2 arms, 2 query inds
+        self.Q_per_action = np.zeros((2, 2))
+
+    def expand(self):
+        for action, query_ind in product((0, 1), (0, 1)):
+            new_history = list(self.actions_history)
+            new_history.append((action, query_ind))
+            new_node = MCTSNode(tuple(new_history), parent=self)
+            self.children[(action, query_ind)] = new_node
+
+    def get_argmax(self):
+        action, query_ind = np.unravel_index(np.argmax(self.Q_per_action, axis=None), self.Q_per_action.shape)
+        return action, query_ind
+
+    def get_action_uct(self, exploration_const):
+        uct_matrix = self.Q_per_action + exploration_const * np.sqrt(np.log(self.N) / self.N_per_action)
+        uct_matrix[np.isnan(uct_matrix)] = np.inf
+        return np.unravel_index(np.argmax(uct_matrix, axis=None), uct_matrix.shape)
+
+
+class MCTree:
+    def __init__(self, actions_history, learning_rate, discount_factor, query_cost, exploration_const):
+        """
+        :param history: A tuple (in order to be hashable) of triplets (action, query_ind, reward)
+        """
+        self.root = MCTSNode(actions_history)
+        self.learning_rate = learning_rate
+        self.discount_factor = discount_factor
+        self.Q = np.random.randn(2, 2)  # 2 arms and 2 queries indicators
+        self.query_cost = query_cost
+        self.exploration_const = exploration_const
+        self.arms_dicts = [{'succ': 0, 'fails': 0}, {'succ': 0, 'fails': 0}]
+
+    def get_arm_dict(self, arm):
+        d = self.arms_dicts[arm]
+        return d
+
+    def update_arm_dict(self, arm, r):
+        """
+        :param arm: which arm was chosen
+        :param r: only calling the function when querying so r in (0, 1)
+        """
+        assert r == 1 or r == 0
+        if r == 1:
+            self.arms_dicts[arm]['succ'] += 1
+        else:
+            self.arms_dicts[arm]['fails'] += 1
+
+    def init_dist_params(self):
+        P_bernoullis = []
+        for i in range(2):  # theta for each of the two arms
+            d = self.get_arm_dict(i)
+            a_i = d['succ']
+            b_i = d['fails']
+            theta = BayesBeta(arm=i, a=a_i, b=b_i).sample()
+            P_bernoullis.append(bernoulli(theta))
+        return P_bernoullis
+
+    @staticmethod
+    def pi_rollout(Q_pi):
+        """
+        :param Q_pi: (2, 2) array actions X query_inds
+        :return: action, query_ind indices
+        """
+        probs = np.exp(Q_pi[:, 0]) / np.sum(np.exp(Q_pi[:, 0]))  # Choosing only arm without quering
+        a_q = np.random.multinomial(1, probs)  # action = flattened array of 0's and 1 in desired action index
+        return np.unravel_index(np.argmax(a_q, axis=None), a_q.shape)[0]
+
+    def tree_search(self, Q_M, max_depth, root, max_simulations=1):
+        if root is None:
+            root = self.root
+        for _ in range(max_simulations):
+            Q_pi = deepcopy(Q_M)
+            P_bernoullis = self.init_dist_params()
+            _ = self.simulate(root, Q_pi, P_bernoullis, max_depth, curr_d=0)
+
+        action, query_ind = root.get_argmax()
+
+        return action, query_ind, root.children[(action, query_ind)]
+
+    def simulate(self, node, Q_pi, P_bernoullis, max_depth, curr_d):
+        if curr_d == max_depth:
+            return 0
+
+        # Rollout
+        if node.N == 0:
+            node.expand()
+            query_ind = 0
+            action = self.pi_rollout(Q_pi)
+            r = P_bernoullis[action].rvs()  # P(;|a,theta)
+            R = r + self.rollout(Q_pi, P_bernoullis, max_depth, curr_d + 1)  # TODO: multiply by discount factor?
+
+            node.N = 1
+            node.N_per_action[action, query_ind] = 1
+            node.Q_per_action[action, query_ind] = R
+            return R
+
+        # Simulation
+        action, query_ind = node.get_action_uct(self.exploration_const)
+        r = P_bernoullis[action].rvs()  # P(;|a,theta)
+        next_node = node.children[(action, query_ind)]
+
+        if query_ind == 0:
+            R = r + self.simulate(next_node, Q_pi, P_bernoullis, max_depth, curr_d + 1)
+        else:
+            R = r + self.simulate(next_node, Q_pi, P_bernoullis, max_depth, curr_d + 1) - self.query_cost
+            # Update Q values for rollout policy
+            self.q_update(Q_pi, action, query_ind, r)
+            # Update rewards history for bayesian update
+            self.update_arm_dict(action, r)
+
+        node.N += 1
+        node.N_per_action[action, query_ind] += 1
+        node.Q_per_action[action, query_ind] += (R - node.Q_per_action[action, query_ind]) / node.N_per_action[action, query_ind]
+        return R
+
+    def rollout(self, Q_pi, P_bernoullis, max_depth, curr_d):
+        if curr_d == max_depth:
+            return 0
+        action = self.pi_rollout(Q_pi)
+        r = P_bernoullis[action].rvs()  # P(;|a,theta)
+        R = r + self.rollout(Q_pi, P_bernoullis, max_depth, curr_d + 1)
+        return R
+
+    def q_update(self, Q_pi, action, query_ind, reward):
+        assert action in (0, 1)
+        assert reward in (0, 1, None)
+        old_val = Q_pi[action, query_ind]
+        Q_pi[action, query_ind] += self.learning_rate * (reward - old_val - self.query_cost + self.discount_factor * max(Q_pi[action, :]))
---
+++ parallel_test_V2.py
@@ -0,0 +1,87 @@
+import argparse
+from tqdm import tqdm
+import csv
+import threading
+import multiprocessing as mp
+from MCTS_V2 import MCTree
+import numpy as np
+from scipy.stats import bernoulli
+from multiprocessing import Pool
+
+# create the lock
+csv_writer_lock = threading.Lock()
+
+
+def parallel_write(writer_path, run, t, arms_thetas, query_cost, T, regret, action, query_ind, r):
+    with open(writer_path, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow(
+            {'run': run, 'timestep': t, 'mus': arms_thetas, 'query_cost': query_cost, 'horizon': T, 'regret': regret,
+             'chosen_arm': action, 'query_ind': query_ind, 'reward': r})
+
+
+def BAMCP_PP(writer_func, writer_path, run, T, learning_rate, discount_factor, query_cost, exploration_const,
+             max_simulations,
+             arms_thetas: tuple, delayed_tree_expansion):
+    """
+    :param T: Horizon
+    :param learning_rate: for Q values
+    :param discount_factor: for Q values
+    :param query_cost: fixed query cost for Q values
+    """
+    actions_history = tuple([])
+    regret = 0
+    Q = np.random.randn(2, 2)
+    mctree = MCTree(actions_history, learning_rate, discount_factor, query_cost, exploration_const)
+    node = None
+    for t in range(T):
+        action, query_ind, node = mctree.tree_search(Q.copy(), max_depth=delayed_tree_expansion, root=node,
+                                                     max_simulations=max_simulations)
+        r = bernoulli(arms_thetas[action]).rvs()
+        if query_ind:
+            mctree.q_update(Q, action, query_ind, r)
+
+        new_history = list(actions_history)
+        new_history.append((action, query_ind))
+        actions_history = tuple(new_history)
+
+        regret += arms_thetas[action] - r
+        with csv_writer_lock:
+            writer_func(writer_path, run, t, arms_thetas, query_cost, T, regret, action, query_ind, r)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--learning_rate', type=float, default=1.,
+                        metavar='')  # No learning rate according to both papers
+    parser.add_argument('--discount_factor', type=float, default=0.95, metavar='')  # According to original BAMCP paper
+    parser.add_argument('--query_cost', type=float, default=0.5, metavar='')  # According to BAMCP++ paper
+    parser.add_argument('--exploration_const', type=float, default=5., metavar='')  # TODO: optimize?
+    parser.add_argument('--max_simulations', type=int, default=100, metavar='')  # TODO: maybe can be higher?
+    parser.add_argument('--arms_thetas', type=tuple, default=(0.2, 0.8), metavar='')  # According to BAMCP++ paper
+    parser.add_argument('--runs', type=int, default=100, metavar='')
+    parser.add_argument('--delayed_tree_expansion', type=int, default=5, metavar='')  # TODO: optimize?
+
+    args = parser.parse_args()
+    num_workers = mp.cpu_count()  # 48
+
+    writer_path = './test_record.csv'
+    with open(writer_path, 'w', newline='') as csvfile:
+        fieldnames = ['run', 'timestep', 'mus', 'query_cost', 'horizon', 'regret', 'chosen_arm', 'query_ind', 'reward']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+    func_args = []
+    for horizon in [60]:  # (10, 20, 30, 40, 50):
+        for query_cost in [0.5]:  # (0, 0.3, 0.5, 1, 100):
+            for run in range(args.runs):
+                func_args.append((parallel_write, writer_path, run, horizon,
+                                  args.learning_rate, args.discount_factor,
+                                  query_cost,
+                                  args.exploration_const, args.max_simulations,
+                                  args.arms_thetas, args.delayed_tree_expansion))
+
+    num_tasks = len(func_args)
+    with Pool(num_workers) as p:
+        for i, _ in enumerate(p.starmap(BAMCP_PP, func_args), 1):
+            print('\rdone {0:.2%}'.format(i / num_tasks))
